fc812957e8ca8f184838214870af7c50afcdf681
LinnierGames <esericksanc@gmail.com>
2020-07-20 22:11:07 -0700

    Get MVP to work

---
+++ My Simple App/URLSession+Fake.swift
@@ -0,0 +1,123 @@
+//
+//  URLSession+Fake.swift
+//  My Simple App
+//
+//  Created by Erick Sanchez on 7/20/20.
+//  Copyright © 2020 Erick Sanchez. All rights reserved.
+//
+
+import Foundation
+
+func injectURLSession() -> URLSession {
+  return SeededURLSession()
+}
+
+class SeededURLSession: URLSession {
+  override func dataTask(
+    with url: URL,
+    completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void
+  ) -> URLSessionDataTask {
+    return SeededDataTask(request: URLRequest(url: url), completion: completionHandler)
+  }
+
+  override func dataTask(
+    with request: URLRequest,
+    completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void
+  ) -> URLSessionDataTask {
+    return SeededDataTask(request: request, completion: completionHandler)
+  }
+}
+
+class SeededDataTask: URLSessionDataTask {
+  private let request: URLRequest
+  private let completion: (Data?, URLResponse?, Error?) -> Void
+  private let realSession = URLSession.shared
+
+  private let fakeResponses = [
+    """
+    {
+      "temperature": 58,
+      "raw-icon": "partly-cloudy-day"
+    }
+    """,
+    """
+    {
+      "temperature": 58,
+      "raw-icon": "clear-day"
+    }
+    """,
+    """
+    {
+      "temperature": 32,
+      "raw-icon": "clear-night"
+    }
+    """,
+    """
+    {
+      "temperature": 54,
+      "raw-icon": "cloudy"
+    }
+    """,
+    """
+    {
+      "temperature": 56,
+      "raw-icon": "fog"
+    }
+    """,
+    """
+    {
+      "temperature": 24,
+      "raw-icon": "partly-cloudy-day"
+    }
+    """,
+    """
+    {
+      "temperature": 64,
+      "raw-icon": "rain"
+    }
+    """,
+    """
+    {
+      "temperature": 23,
+      "raw-icon": "sleet"
+    }
+    """,
+    """
+    {
+      "temperature": 21,
+      "raw-icon": "snow"
+    }
+    """,
+    """
+    {
+      "temperature": 86,
+      "raw-icon": "thunder"
+    }
+    """,
+    """
+    {
+      "temperature": 42,
+      "raw-icon": "unbrella"
+    }
+    """,
+    """
+    {
+      "temperature": 46,
+      "raw-icon": "windy"
+    }
+    """
+  ]
+
+  init(request: URLRequest, completion: @escaping (Data?, URLResponse?, Error?) -> Void) {
+    self.request = request
+    self.completion = completion
+  }
+
+  override func resume() {
+    let response = HTTPURLResponse(url: request.url!, statusCode: 200, httpVersion: nil, headerFields: nil)
+    let data = fakeResponses.randomElement()!.data(using: .utf8)
+    completion(data, response, nil)
+  }
+}
+
+
---
+++ My Simple App/UnknownTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  UnknownTableViewCell.swift
+//  My Simple App
+//
+//  Created by Erick Sanchez on 7/20/20.
+//  Copyright © 2020 Erick Sanchez. All rights reserved.
+//
+
+import UIKit
+
+class UnknownTableViewCell: UITableViewCell {
+
+  @IBOutlet weak var titleLabel: UILabel!
+
+  override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
