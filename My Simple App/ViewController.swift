6171757345fa2a9cac3324565c1997d2954d443d
LinnierGames <esericksanc@gmail.com>
2020-07-20 18:57:14 -0700

    Initial Commit

---
+++ My Simple App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  My Simple App
+//
+//  Created by Erick Sanchez on 7/20/20.
+//  Copyright © 2020 Erick Sanchez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
---
+++ My Simple AppTests/My_Simple_AppTests.swift
@@ -0,0 +1,34 @@
+//
+//  My_Simple_AppTests.swift
+//  My Simple AppTests
+//
+//  Created by Erick Sanchez on 7/20/20.
+//  Copyright © 2020 Erick Sanchez. All rights reserved.
+//
+
+import XCTest
+@testable import My_Simple_App
+
+class My_Simple_AppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
