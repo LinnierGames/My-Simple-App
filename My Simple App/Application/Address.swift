963b435415095b1a7250481547f3a7674fa75fa1
LinnierGames <esericksanc@gmail.com>
2020-08-05 18:10:34 -0700

    Move data models closer to services

--- My Simple App/Application/Address.swift
+++ My Simple App/Application/Address.swift
@@ -1,8 +1,8 @@
 //
-//  WeatherModels.swift
+//  Address.swift
 //  My Simple App
 //
-//  Created by Erick Sanchez on 7/23/20.
+//  Created by Erick Sanchez on 8/5/20.
 //  Copyright © 2020 Erick Sanchez. All rights reserved.
 //
 
@@ -15,22 +15,12 @@ class Address {
   var weather: Weather?
 }
 
-struct WeatherIcon {
-  let name: String
-  let image: UIImage
-}
-
 struct Weather {
   let temperature: CGFloat
   let icon: WeatherIcon
 }
 
-struct WeatherData: Decodable {
-  let temperature: Float
-  let rawIcon: String
-
-  enum CodingKeys: String, CodingKey {
-    case temperature
-    case rawIcon = "raw-icon"
-  }
+struct WeatherIcon {
+  let name: String
+  let image: UIImage
 }
